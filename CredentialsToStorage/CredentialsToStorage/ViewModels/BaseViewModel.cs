455420053583b0a70e3903c2481173f50869ce40
patrickfonseca208 <patrickfonseca208@gmail.com>
2023-06-14 02:54:22 -0300

    Update repository

--- CredentialsToStorage/CredentialsToStorage/ViewModels/BaseViewModel.cs
+++ CredentialsToStorage/CredentialsToStorage/ViewModels/BaseViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
@@ -22,13 +21,10 @@ namespace CredentialsToStorage.ViewModels
             protected set { SetProperty(ref _title, value); }
         }
 
-        protected bool SetProperty<T>(ref T backingStore, T value,
-            [CallerMemberName] string propertyName = "",
-            Action onChanged = null)
+        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "")
         {
             if (EqualityComparer<T>.Default.Equals(backingStore, value)) { return false; }
             backingStore = value;
-            onChanged?.Invoke();
             OnPropertyChanged(propertyName);
             return true;
         }
@@ -37,9 +33,7 @@ namespace CredentialsToStorage.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
-            var changed = PropertyChanged;
-            if (changed == null) { return; }
-            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
     }
